c6e369e05358c231edac1332965f02e4bc6475cf
Simon-Gab <Cloutier.simongabriel@gmail.com>
2024-12-24 14:12:30 -0500

    test1

--- components/Menu.jsx
+++ components/Menu.jsx
@@ -29,32 +29,8 @@ export default function Menu() {
                         aria-label="Contacts"
                         href="/contacts"></Link>
                     </li>
-                    <li>
-                        <Link className={`
-                        ${styles.grow}
-                        ${styles.li}
-                        ${styles.toMy} 
-                        ${styles.project}`}
-                        aria-label="Projects"
-                        href="/projects_display"></Link>
-                    </li>
-                    <li>
-                        <Link className={`
-                        ${styles.grow}
-                        ${styles.li} 
-                        ${styles.universe} 
-                        ${styles.about}`}
-                        aria-label="About me"
-                        href="/about"></Link>
-                    </li>
                 </ul>
             </nav>
-    {/*Image of me*/}
-            <div className={`${styles.div}`}>
-                <Link href="/404">
-                    <Image src={monStyle} alt="Moi" className={`${styles.img} ${styles.jump}`} priority />
-                </Link>
-            </div>
         </section>
     </>
 }
